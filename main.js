8443774cccaf994befd5f03f1bc4b45c8cee7755
Windows <alexdossantos3077@gmail.com>
2023-11-13 10:24:40 -0300

    new menu, new system partiule in develop

--- main.js
+++ main.js
@@ -9,13 +9,10 @@ Sound.setVolume(100);
 Sound.setVolume(100,0);
 
 let sounds={
-  ball_audio: Sound.load("assets/sound/ball_to_wall.adp"),
-  thema: Sound.load("assets/sound/theme.adp"),
-  youlose: Sound.load("assets/sound/youloser.adp"),
-  winner: Sound.load("assets/sound/youwin.adp"),
-  trobeta: Sound.load("assets/sound/snd09.adp")
+  ball_audio: Sound.load("assets/sound/snd13.wav")
 };
 
+  
 const colors = {
   Black: Color.new(0, 0, 0),
   White: Color.new(255, 255, 255),
@@ -23,9 +20,13 @@ const colors = {
   Blue: Color.new(48, 92, 255)
 };
 
-
+let particles = [];
+let imagePart = new Image("assets/effect/light_obstacle.png");
 const MenuImage = {
   menu: new Image("assets/mainmenu/mainmenu.png"),
+  menu_pause: new Image("assets/mainmenu/menu_pause.png"),
+  menu_opçoes: new Image("assets/mainmenu/menu_opcões.png"),
+  hand_difficulty: new Image("assets/mainmenu/difficulty.png"),
   seta: new Image("assets/mainmenu/Check.png")
 };
 var Players = {
@@ -39,8 +40,7 @@ const GameImage = {
   red: new Image("assets/game/red_paddle.png"),
   blue: new Image("assets/game/blue_paddle.png"),
   winner: new Image("assets/game/result/result_text_youwin.png"),
-  loser: new Image("assets/game/result/result_text_youlose.png"),
-  
+  loser: new Image("assets/game/result/result_text_youlose.png")
 };
 
 const Black = Color.new(255, 255, 255);
@@ -52,30 +52,38 @@ var screen = 0;
 // variaveis do jogo
 var Ball = { X: 285, Y: 190};
 // eixo anolosico da maxinha
-var minOriginal = -127;
-var maxOriginal = 128;
+var minOriginal = 0;
+var maxOriginal = 640;
 let new_pad = Pads.get();
 let old_pad = new_pad;
 let pd = Pads.get();
 let pd2 = Pads.get();
 var velocidade = 8;
-var ballSpeedX = 5;
-var ballSpeedY = 5;
+var ballSpeedX = 2;
+var ballSpeedY = 2;
 let Nums = {
   nums_red : new Image("assets/num/num_blue_"+Players.Player2[0].gols+".png"),
   nums_blue : new Image( "assets/num/num_blue_"+Players.Player1[0].gols+".png")
 }
+var op_seta = 
+  [{ x: 388, y: 317 },
+  { x: 388, y: 357 },
+  { x: 388, y: 397 },
+  { x: 388, y: 397 },
+  { x: 388, y: 397 }]
 var seta = 
   [{ x: 388, y: 317 },
   { x: 388, y: 357 },
   { x: 388, y: 397 }];
+
 let seta_pos = seta[0] 
 var Count = 0;
 var selected = 0;
-
+var difficulty = 0;
 
 
 class main {
+  
   SetScreen() {
     if (screen == 0) {
       this.Menu();
@@ -84,10 +92,27 @@ class main {
       this.Play();
     }
     if (screen == 2){
-      this.Credits();
+      this.menu_opçoes();
+    }
+    if (screen == 3){
+      this.menu_pause();
     }
   }
-  
+  createParticle(x, y, dx, dy, imagePart) {
+    particles.push({ x, y, dx, dy, imagePart });
+  }
+  updateParticles() {
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].x += particles[i].dx;
+      particles[i].y += particles[i].dy;
+    }
+  }
+  drawParticles() {
+    for (let i = 0; i < particles.length; i++) {
+      const { x, y, imagePart } = particles[i];
+      imagePart.draw(x,y);
+    }
+  }
   ResetPlayers(){
     Players.Player1[0].X = 569;
     Players.Player1[0].Y = 195;
@@ -171,8 +196,8 @@ class main {
       return 0;
     }
   }
-  Credits(){
-    screen = 0;
+  Opcoes(){
+    screen = 2;
   }
   Menu() {
     old_pad = new_pad;
@@ -199,6 +224,69 @@ class main {
     Sound.play(sounds.thema);
     MenuImage.menu.draw(0, 0);
     MenuImage.seta.draw(seta_pos.x, seta_pos.y);
+  }
+
+  menu_pause(){
+    MenuImage.menu_pause.draw(0,0);
+    MenuImage.seta.draw(seta_pos.x,seta_pos.y);
+    old_pad = new_pad;
+    new_pad = Pads.get();
+
+    if (Pads.check(new_pad, Pads.UP) && !Pads.check(old_pad, Pads.UP)) {
+      this.MoveSetaUp();
+    }
+    if (Pads.check(new_pad, Pads.DOWN) && !Pads.check(old_pad, Pads.DOWN)) {
+      this.MoveSetaDown();
+    }
+    if (Pads.check(new_pad, Pads.CROSS) && !Pads.check(old_pad, Pads.CROSS) && selected == 0) {
+      screen = 1;
+      this.ResetBall();
+      this.ResetPlayers();
+    }
+    if (Pads.check(new_pad, Pads.CROSS) && !Pads.check(old_pad, Pads.CROSS) && selected == 1) {
+      screen = 1;
+      this.ResetBall();
+      this.ResetPlayers();
+      Players.Player1[0].gols = 0;
+      Players.Player2[0].gols = 0;
+    }
+    if (Pads.check(new_pad, Pads.CROSS) && !Pads.check(old_pad, Pads.CROSS) && selected == 2) {
+      screen = 0;
+      
+    }
+
+    
+  }
+
+  menu_opçoes(){
+    atual = 0;
+    MenuImage.menu_opçoes.draw(0,0);
+    MenuImage.hand_difficulty.draw(200,200);
+    difficulty = 0;
+    let pos_seta = op_seta[0];
+    MenuImage.seta.draw(pos_seta.x,pos_seta.y);
+    old_pad = new_pad;
+    new_pad = Pads.get();
+
+    if (Pads.check(new_pad, Pads.UP) && !Pads.check(old_pad, Pads.UP)) {
+      if (atual > 0){
+        pos_seta = op_seta[atual -= 1];
+      }else{
+        pos_seta = op_seta[4];
+        difficulty = 4;
+        atual =0;
+      }
+    }
+    if (Pads.check(new_pad, Pads.DOWN) && !Pads.check(old_pad, Pads.DOWN)) {
+      if (atual < 5){
+        pos_seta = op_seta[atual += 1];
+      }else{
+        pos_seta = op_seta[0];
+        difficulty = 0;
+        atual
+      }
+    }
+    
   }
 
   Move_paddles() {
@@ -238,6 +326,7 @@ class main {
     GameImage.blue.draw(Players.Player2[0].X, Players.Player2[0].Y);
     Nums.nums_blue.draw(272,25);
     Nums.nums_red.draw(330,25);
+    
   }
 
   start() {
@@ -245,8 +334,11 @@ class main {
     new_pad = Pads.get(0);
 
     if (Pads.check(new_pad, Pads.START) && !Pads.check(old_pad, Pads.START) && selected == 0) {
-      screen = 0;
+     screen = 3;
     }
+    if (Pads.check(new_pad, Pads.START) && !Pads.check(old_pad, Pads.START) && selected == 1) {
+      screen = 3;
+     }
   }
   WinnerPlayer(){
     if(Players.Player1[0].gols == 10){
@@ -264,14 +356,23 @@ class main {
     }
   }
   CollisionBall() {
+    if (Ball.X + 70 > canvas.width || Ball.X < -2) {
+      this.ResetBall();
+    }
+    if (Ball.Y + 70 > canvas.height || Ball.Y < -2) {
+      this.ResetBall();
+    }
     // Verificar colisão com as bordas da tela para a bola
     if (Ball.X + 64 >= canvas.width || Ball.X <= 0) {
       ballSpeedX = -ballSpeedX; // Inverter a direção no eixo X
-      Sound.play(sounds.ball_audio);
+      createParticle(100, 100, 2, 2);
+      createParticle(200, 200, -1, 1);
+      
     }
     if (Ball.Y + 64 >= canvas.height || Ball.Y <= 0) {
       ballSpeedY = -ballSpeedY; // Inverter a direçãeixo Yo no
-      Sound.play(sounds.ball_audio);
+      createParticle(100, 100, 2, 2);
+      createParticle(200, 200, -1, 1);
     }
     //Colisão dos paddle com a parede
     if (Players.Player1[0].X < 320) {
@@ -312,14 +413,12 @@ class main {
     if (// paddle esq
     Ball.X + 64 >= Players.Player2[0].X && Ball.X <= Players.Player2[0].X + 100 && Ball.Y + 64 >= Players.Player2[0].Y && Ball.Y <= Players.Player2[0].Y + 100
     ) {
-      Sound.play(sounds.ball_audio);
       ballSpeedY = this.normalizeValue(pd.ry, minOriginal, maxOriginal);
       ballSpeedX = this.normalizeValue(pd.rx, minOriginal, maxOriginal);
     }
     if (Ball.X <= Players.Player1[0].X + 100 && Ball.X + 64 >= Players.Player1[0].X && Ball.Y + 64 >= Players.Player1[0].Y && Ball.Y <= Players.Player1[0].Y + 100) {
-      Sound.play(sounds.ball_audio);
       ballSpeedY = this.normalizeValue(pd2.ry, minOriginal, maxOriginal);
-      ballSpeedX = this.normalizeValue(pd2.rx, minOriginal, maxOriginal);
+      ballSpeedX = this.normalizeValue(pd2.ry, minOriginal, maxOriginal);
       if(selected == 1){
         ballSpeedY = this.normalizeValue(Players.Player1[0].Y, minOriginal, maxOriginal);
         ballSpeedX = this.normalizeValue(Players.Player1[0].X, minOriginal, maxOriginal);
@@ -349,7 +448,6 @@ class main {
   }
 
   Play() {
-    this.start();
     this.Move_paddles();
     this.MoveBall();  // Adicionando a movimentação da bola
     this.CollisionBall();
@@ -357,6 +455,9 @@ class main {
     this.DesacelateBall();
     this.Check_gol();
     this.WinnerPlayer();
+    this.updateParticles();
+    this.drawParticles();
+    
     
   }
 }
